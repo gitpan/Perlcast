d0bdce7c054b09257dcdb8eb47a5b8a778d4a5de
Joshua McAdams <jmcada@cpan.org>
2006-01-05 04:59:16 +0000

    Import of JMCADA/Perlcast-20060104 from CPAN.
    gitpan-cpan-distribution: Perlcast
    gitpan-cpan-version:      20060104
    gitpan-cpan-path:         JMCADA/perlcast/Perlcast-20060104.tar.gz
    gitpan-cpan-author:       JMCADA
    gitpan-cpan-maturity:     released

--- lib/Perlcast.pm
+++ lib/Perlcast.pm
@@ -9,54 +9,20 @@ Perlcast - Transcriptions of Perlcast
 
 =head1 VERSION
 
-Version 20051209 
+Version 20060104 
 
 =cut
 
-our $VERSION = '20060103';
+our $VERSION = '20060104';
 
 =head1 SYNOPSIS
 
 The module really doesn't do anything except provide transcriptions of 
 Perlcast episodes and interviews.
 
-=head1 INTERVIEWS
+=head1 CONTENTS
 
-=over 4
-
-=item L<Perlcast::Interviews::001> - brian d foy on I<The Perl Review>
-
-=item L<Perlcast::Interviews::002> - Ian Langworth and chromatic on I<Perl Testing: A Developer's Notebook>
-
-=item L<Perlcast::Interviews::003> - Damian Conway on I<Perl Best Practices>
-
-=item L<Perlcast::Interviews::004> - Peter Scott on I<Perl Medic>
-
-=item L<Perlcast::Interviews::005> - Chris Brooks on Talkr
-
-=item L<Perlcast::Interviews::006> - Casey West on JSAN
-
-=item L<Perlcast::Interviews::007> - Adam Kennedy on PPI
-
-=item L<Perlcast::Interviews::008> - Shane Caraveo on ActiveState's Komodo
-
-=item L<Perlcast::Interviews::009> - Jesse Vincent on RT
-
-=item L<Perlcast::Interviews::010> - Larry Wall on Perl
-
-=item L<Perlcast::Interviews::011> - Andy Lester on The Perl Foundation
-
-=item L<Perlcast::Interviews::012> - Chris Frenz on I<Pro Perl Parsing>
-
-=item L<Perlcast::Interviews::013> - Randal Schwartz on Perl
-
-=item L<Perlcast::Interviews::014> - Richard Foley on I<Pro Perl Debugging>
-
-=item L<Perlcast::Interviews::015> - Peter Wainwright on I<Pro Perl>
-
-=item L<Perlcast::Interviews::016> - Jason Stajich on BioPerl 
-
-=back
+L<Interviews|Perlcast::Interviews>
 
 =head1 AUTHOR
 
@@ -112,7 +78,7 @@ The transcription effort has been driven by...
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2005 Josh McAdams, all rights reserved.
+Copyright 2005-2006 Josh McAdams, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
